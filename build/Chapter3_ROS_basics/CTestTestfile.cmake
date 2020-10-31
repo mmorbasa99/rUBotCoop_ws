7e837070351064659aa105e4e98c36bd59722010
manelpuig <manel.puig@ub.edu>
2020-10-31 13:37:49 +0100

    Saturday1

---
+++ build/Chapter3_ROS_basics/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mpuig/catkin_ws/src/Chapter3_ROS_basics
+# Build directory: /home/mpuig/catkin_ws/build/Chapter3_ROS_basics
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/mpuig/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/mpuig/catkin_ws/devel;/home/mpuig/rUBot_SW/devel;/opt/ros/melodic'.split(';') if '/home/mpuig/catkin_ws/devel;/home/mpuig/rUBot_SW/devel;/opt/ros/melodic' != '' else []
