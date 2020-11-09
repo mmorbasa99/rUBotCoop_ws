87a7e92aa2910fad424f063f4be2804e427beb7f
Manel Puig <manel.puig@ub.edu>
2020-11-09 17:19:46 +0100

    Monday1

---
+++ build/ros_basics/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mpuig/rUBotCoop_ws/src/ros_basics
+# Build directory: /home/mpuig/rUBotCoop_ws/build/ros_basics
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/turtlesim_tutorial/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mpuig/rUBotCoop_ws/src/turtlesim_tutorial
+# Build directory: /home/mpuig/rUBotCoop_ws/build/turtlesim_tutorial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
